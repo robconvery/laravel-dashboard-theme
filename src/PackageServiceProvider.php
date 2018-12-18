9c57c122d72f5dae871c479ce51a07e5c9f7b48e
Robert Convery <robconvery@me.com>
2018-12-18 19:42:17 +0000

    Added slash

--- src/PackageServiceProvider.php
+++ src/PackageServiceProvider.php
@@ -15,11 +15,11 @@ class PackageServiceProvider extends ServiceProvider
     {
         $this->publishes([
             // publish layouts
-            dirname(__DIR__) .'/assets/views/layouts' => public_path('resources/views/layouts/dashboard'),
+            dirname(__DIR__) .'/assets/views/layouts' => public_path('/resources/views/layouts/dashboard'),
             // publish example logo
-            dirname(__DIR__) .'/assets/images' => public_path('resources/images/dashboard'),
+            dirname(__DIR__) .'/assets/images' => public_path('/resources/images/dashboard'),
             // publish sass
-            dirname(__DIR__) .'/assets/sass' => public_path('resources/sass/dashboard'),
+            dirname(__DIR__) .'/assets/sass' => public_path('/resources/sass/dashboard'),
             // publish js
             dirname(__DIR__) .'/assets/js' => public_path('resources/js/dashboard')
         ], 'dashboard');
