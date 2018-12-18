6233f7ab0c34472b1fcb9f57362370af0381e501
Robert Convery <robconvery@me.com>
2018-12-18 22:11:30 +0000

    Updated the publishing command

--- src/PackageServiceProvider.php
+++ src/PackageServiceProvider.php
@@ -15,13 +15,13 @@ class PackageServiceProvider extends ServiceProvider
     {
         $this->publishes([
             // publish layouts
-            dirname(__DIR__) .'/assets/views/layouts' => resource_path('views/layouts/dashboard'),
+            dirname(__DIR__) . '/assets/views/layouts' => resource_path('views/layouts/dashboard'),
             // publish example logo
-            dirname(__DIR__) .'/assets/images' => resource_path('images/dashboard'),
+            dirname(__DIR__) . '/assets/images' => resource_path('assets/dashboard'),
             // publish sass
-            dirname(__DIR__) .'/assets/sass' => resource_path('sass/dashboard'),
+            dirname(__DIR__) . '/assets/sass' => resource_path('sass/dashboard'),
             // publish js
-            dirname(__DIR__) .'/assets/js' => resource_path('js/dashboard')
+            dirname(__DIR__) . '/assets/js' => resource_path('js/dashboard')
         ], 'dashboard');
     }
 
