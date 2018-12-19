c0e0669806f4884f5a3927dff9fe9a252686baa5
Robert Convery <robconvery@me.com>
2018-12-19 00:09:28 +0000

    Updated dashboard template

--- assets/views/layouts/dashboard.blade.php
+++ assets/views/layouts/dashboard.blade.php
@@ -20,53 +20,70 @@
 
 <body>
 
-    <div>
+<nav class="navbar navbar-expand-lg navbar-light bg-navbar">
 
-        <nav class="navbar navbar-expand-lg navbar-light bg-navbar border-bottom">
+    <a class="navbar-brand" href="{{ route('home') }}">
+        <img src="{{ asset('images/logo.jpg') }}" alt="{{ config('app.name') }}" class="nav-logo">
+        {{ config('app.name') }}
+    </a>
 
-            <a class="navbar-brand" href="{{ route('home') }}">
-                <img src="/images/logo.jpg" class="nav-logo">
-                {{ config('app.name') }}
-            </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 
-            <div class="ml-auto">
-                <a class="nav-link" href="{{ route('logout') }}">
-                    <i class="fas fa-sign-out-alt"></i>
-                    Logout
+        <span class="navbar-toggler-icon"></span>
+
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+
+        <ul class="navbar-nav mr-auto">
+
+            <li class="nav-item active">
+                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link" href="#">Link</a>
+            </li>
+
+            <li class="nav-item dropdown">
+
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Dropdown
                 </a>
-            </div>
 
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-
-        </nav>
-
-        <div class="container-fluid">
-
-            <div class="row">
-
-                <div class="collapse navbar-collapse sidebar" id="navbarSupportedContent">
-
-                    <ul class="navbar-nav mr-auto">
-
-                        <li class="nav-item{{{ Request::is('dashboard') ? ' active' : '' }}}">
-                            <a class="nav-link border-bottom" href="{{ route('home') }}">
-                                <i class="far fa-calendar-alt"></i>
-                                Diary
-                            </a>
-                        </li>
-
-                    </ul>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="#">Action</a>
+                    <a class="dropdown-item" href="#">Another action</a>
+                    <div class="dropdown-divider"></div>
+                    <a class="dropdown-item" href="#">Something else here</a>
                 </div>
 
-                <main role="main" class="col-lg-10 ml-lg-auto px-4 bg-dashboard border-left">
+            </li>
 
-                    @yield('content')
+            <li class="nav-item">
+                <a class="nav-link disabled" href="#">Disabled</a>
+            </li>
 
-                </main>
-            </div>
-        </div>
+        </ul>
+
+        <form class="form-inline my-2 my-lg-0">
+            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">
+                Search
+            </button>
+        </form>
+
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" href="{{ route('logout') }}">Logout</a>
+            </li>
+        </ul>
+    </div>
+</nav>
+
+<div class="bg-white">
+    @yield('content')
+</div>
 
 <script src="{{ asset('js/app.js') }}"></script>
 
