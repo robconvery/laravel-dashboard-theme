9ea1ece7b80733f0baf3769255d6b0fefd133753
Robert Convery <robconvery@me.com>
2018-12-21 23:43:19 +0000

    Fixed copy location of file

--- src/PackageServiceProvider.php
+++ src/PackageServiceProvider.php
@@ -15,7 +15,7 @@ class PackageServiceProvider extends ServiceProvider
     {
         $this->publishes([
             // publish layouts
-            dirname(__DIR__) . '/assets/views/layouts' => resource_path('views/layouts/dashboard'),
+            dirname(__DIR__) . '/assets/views/layouts' => resource_path('views/layouts/vendor/dashboard'),
             // publish example logo
             dirname(__DIR__) . '/assets/images' => resource_path('assets/dashboard'),
             // publish sass
